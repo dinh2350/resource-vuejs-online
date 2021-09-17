07f60bfcfe70e848e8259f9e8df75d2d7f8bcf96
dinh2350 <hao9x015@mail.com>
2021-09-17 15:42:36 +0700

    app-user-management

---
+++ chapter-3-vuex-and-vue-form/app-user-management/src/components/UserList.vue
@@ -0,0 +1,35 @@
+<template>
+  <tbody>
+    <user-item
+      v-for="(user, index) in userListBySearchName"
+      :key="index"
+      :user="user"
+    ></user-item>
+  </tbody>
+</template>
+
+<script>
+import UserItem from "./UserItem.vue";
+import { mapGetters, mapState } from "vuex";
+export default {
+  components: {
+    UserItem,
+  },
+  // computed: mapState(["userList"]),
+  computed: {
+    loading() {
+      return false;
+    },
+    ...mapState({
+      userList: (state) => state.userList,
+      // users: "userList",
+    }),
+    ...mapGetters({
+      userListFilterBoy: "userListByBoy",
+      userListBySearchName: "userListBySearchName",
+    }),
+  },
+};
+</script>
+
+<style></style>
