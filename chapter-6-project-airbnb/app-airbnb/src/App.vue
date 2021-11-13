c4acaccd6d4c82e79eba3b9a05d75470b397e7f0
dinh2350 <hao9x015@mail.com>
2021-11-13 15:40:15 +0700

    push code

---
+++ chapter-6-project-airbnb/app-airbnb/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <the-header></the-header>
+  <router-view />
+  <the-footer></the-footer>
+</template>
+
+<script>
+import TheHeader from "./components/TheHeader.vue";
+import TheFooter from "./components/TheFooter.vue";
+export default {
+  components: {
+    TheHeader,
+    TheFooter,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ chapter-6-project-airbnb/app-airbnb/src/components/RoomListItems.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="col-lg-12 col-md-12">
+    <div class="listing-item-container list-layout" data-marker-id="2">
+      <a href="listings-single-page.html" class="listing-item">
+        <!-- Image -->
+        <div class="listing-item-image">
+          <img src="images/listing-item-02.jpg" alt="" />
+          <span class="tag">Events</span>
+        </div>
+
+        <!-- Content -->
+        <div class="listing-item-content">
+          <div class="listing-item-inner">
+            <h3>Sticky Band</h3>
+            <span>Bishop Avenue, New York</span>
+            <div class="star-rating" data-rating="5.0">
+              <div class="rating-counter">(23 reviews)</div>
+            </div>
+          </div>
+
+          <span class="like-icon"></span>
+
+          <div class="listing-item-details">Friday, August 10</div>
+        </div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
